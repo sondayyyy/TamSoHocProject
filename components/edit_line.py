b211b3a58cc77a51b6f01f710565969a26391153
TamSoHocProject <thanhson09122002@gmail.com>
2024-10-23 00:31:31 +0700

    Le_Widget_update

--- components/edit_line.py
+++ components/edit_line.py
@@ -1,31 +1,38 @@
-from PySide6.QtCore import  QObject, QThread, Signal, Qt
-from PySide6.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QWidget, QLineEdit
-# from PyQt6.QtGui import QDoubleValidator, QIntValidator
-import threading
-from threading import Thread
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from ui import Ui_MainWindow
+from PySide6.QtWidgets import QLineEdit
+from PySide6.QtGui import QIntValidator, QRegularExpressionValidator
+from PySide6.QtCore import QRegularExpression
 
 class CustomLineEdit(QLineEdit):
     def __init__(self, parent=None):
         super(CustomLineEdit, self).__init__(parent)
-        self.qline_edit = Ui_MainWindow.setupUi(self.le_main_search_time)
         
+    def setup_name_edit(self):
+        self.setMaxLength(50)  # Giới hạn số ký tự
+        # Thiết lập validator cho tên
+        name_regex = QRegularExpression("[A-Za-z ]+")
+        name_validator = QRegularExpressionValidator(name_regex, self)
+        self.setValidator(name_validator)
 
-    def qline_edit_name(self):
-        self.qline_edit.le_main_search_name.setMaxLength(50)
+    def setup_day_edit(self):
+        self.setMaxLength(2)  # Giới hạn 2 ký tự
+        # Thiết lập validator cho ngày
+        day_validator = QIntValidator(1, 31, self)
+        self.setValidator(day_validator)
 
-    def qline_edit_day(self):
-        self.qline_edit.le_main_search_day.setMaxLength(2)
+    def setup_month_edit(self):
+        self.setMaxLength(2)  # Giới hạn 2 ký tự
+        # Thiết lập validator cho tháng
+        month_validator = QIntValidator(1, 12, self)
+        self.setValidator(month_validator)
 
-    def qline_edit_month(self):
-        # month_regex = QIntValidator(1, 12)  # 1 to 12
-        self.qline_edit.le_main_search_month.setMaxLength(2)
+    def setup_year_edit(self):
+        self.setMaxLength(4)  # Giới hạn 4 ký tự
+        # Thiết lập validator cho năm
+        year_validator = QIntValidator(1900, 2100, self)
+        self.setValidator(year_validator)
 
-    def qline_edit_year(self):
-        self.qline_edit.le_main_search_year.setMaxLength(4)
-
-    def qline_edit_time(self):
-        # Hour QLineEdit - Allow numbers between 0 and 23
-        self.qline_edit.le_main_search_time.setPlaceholderText(0, 23)
+    def setup_time_edit(self):
+        self.setMaxLength(2)  # Giới hạn 2 ký tự
+        # Thiết lập validator cho giờ
+        time_validator = QIntValidator(0, 23, self)
+        self.setValidator(time_validator)
